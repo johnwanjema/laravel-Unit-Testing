67fae498005742ba4a71f9e10da335ed92d5e259
John Wanjema <jonwanjema@gmail.com>
2021-07-10 11:44:35 +0300

    specify fiesld

--- database/factories/CEOFactory.php
+++ database/factories/CEOFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'company_name' => $faker->unique()->company,
+        'year' => $faker->year,
+        'company_headquarters' => $faker->city,
+        'what_company_does' => $faker->sentence
     ];
 });
